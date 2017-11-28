5604cc1dbc358b10edac76ba5c8051da8d3b6b21
Sar-Kerson <yaoleiqi@qq.com>
2017-11-28 09:48:52 +0800

    basic dehaze processor

--- include/dehazeProcessor.h
+++ include/dehazeProcessor.h
@@ -5,7 +5,9 @@ class dehazeProcessor
 public:
 	void setInput(const cv::Mat& input);
 
-	void setFileName(cv::String filename);
+	void setInput(const cv::String & filename);
+
+	// void setFileName(cv::String filename);
 
 	void getOutput(cv::Mat & output);
 
@@ -13,17 +15,21 @@ public:
 
 	dehazeProcessor(const cv::String & filename);
 
-	dehazeProcessor(const cv::Mat & srcImg, const cv::String & filename);
+	// dehazeProcessor(const cv::Mat & srcImg, const cv::String & filename);
 
 	dehazeProcessor() {}
 
+	cv::Mat & src() { return this->srcImg; }
+	cv::Mat & dst() { return this->dstImg; }
+
 public:
 	virtual void process() = 0;
+	virtual void hazeFree() = 0;
 
 private:
 	cv::Mat srcImg;
 	cv::Mat dstImg;
-	cv::String filename;
+	// cv::String filename;
 };
 
 void hazeFree(cv::Mat & srcImg, cv::Mat & output, int type);
