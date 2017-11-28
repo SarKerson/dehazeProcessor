c11fe03cf7830bbbd62b7b1ad6e16a38d5d0f513
Sar-Kerson <yaoleiqi@qq.com>
2017-11-28 09:51:46 +0800

    basic dehaze processor

--- src/dehazeProcessor.cpp
+++ src/dehazeProcessor.cpp
@@ -2,29 +2,40 @@
 
 dehazeProcessor::dehazeProcessor(const cv::Mat& srcImg)
 {
-	this->srcImg = srcImg.clone();
+	this->setInput(srcImg);
 }
 
 dehazeProcessor::dehazeProcessor(const cv::String & filename)
 {
-	this->filename = filename;
+	this->setInput(filename);
 }
 
-dehazeProcessor::dehazeProcessor(const cv::Mat & srcImg, const cv::String & filename)
-{
-	this->filename = filename;
-	this->srcImg = srcImg.clone();
-}
+// dehazeProcessor::dehazeProcessor(const cv::Mat & srcImg, const cv::String & filename)
+// {
+// 	this->filename = filename;
+// 	this->srcImg = srcImg.clone();
+// }
 
 void dehazeProcessor::setInput(const cv::Mat& input)
 {
 	this->srcImg = input.clone();
 }
 
-void dehazeProcessor::setFileName(cv::String filename)
+void dehazeProcessor::setInput(const cv::String & filename)
 {
-	this->filename = filename;
+	try {
+		this->srcImg = cv::imread(filename);
+	}
+	catch (cv::Exception e) {
+		std::cout << e.what() << std::endl;
+		exit(1);
+	}
 }
+
+// void dehazeProcessor::setFileName(cv::String filename)
+// {
+// 	this->filename = filename;
+// }
 
 void dehazeProcessor::getOutput(cv::Mat & output)
 {
