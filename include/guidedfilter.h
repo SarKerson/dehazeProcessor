a20e07a5731a83dcc52ea3377cfb2419db8bc769
Sar-Kerson <yaoleiqi@qq.com>
2017-11-28 09:52:41 +0800

    the guided filter

---
+++ include/guidedfilter.h
@@ -0,0 +1,22 @@
+#ifndef __GUIDED_FILTER_H__
+#define __GUIDED_FILTER_H__
+
+#include <opencv2/opencv.hpp>
+
+class GuidedFilterImpl;
+
+class GuidedFilter
+{
+public:
+    GuidedFilter(const cv::Mat &I, int r, double eps);
+    ~GuidedFilter();
+
+    cv::Mat filter(const cv::Mat &p, int depth = -1) const;
+
+private:
+    GuidedFilterImpl *impl_;
+};
+
+cv::Mat guidedFilter(const cv::Mat &I, const cv::Mat &p, int r, double eps, int depth = -1);
+
+#endif
