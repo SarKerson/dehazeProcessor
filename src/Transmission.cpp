36458acece5642cf6f8dbbe54d0acc1b360ab726
Sar-Kerson <512529447@qq.com>
2017-12-05 09:11:10 +0800

    commit

--- src/Transmission.cpp
+++ src/Transmission.cpp
@@ -22,45 +22,48 @@ void Transmission::calTransmission(cv::Mat & srcImg,
 		srcImg.convertTo(img_scale, CV_32FC3, 1.0 / 255, 0);
 	else
 		img_scale = srcImg;
-	// this->t.create(img_scale.size(), CV_32FC1);
 	std::vector<float> max_dist(NUM_SPH);
     float ** dist = new float*[img_scale.rows];
-    int ** img_index = new int*[img_scale.rows];
     for (int i = 0; i < img_scale.rows; ++i) {
         dist[i] = new float[img_scale.cols];
-        img_index[i] = new int[img_scale.cols];
     }
-    cv::Mat T(img_scale.size(), CV_32FC1), img_refine(img_scale.size(), CV_32FC3);/*haze_img - A;*/
+    float * query_ar = new float[img_scale.rows * img_scale.cols * 2];
+    int count = 0;
+    cv::Mat T(img_scale.size(), CV_32FC1);
     for (int i = 0; i < img_scale.rows; ++i) {
         for (int j = 0; j < img_scale.cols; ++j) {
             cv::Vec3f & pixel = img_scale.at<cv::Vec3f>(i, j);
             cv::Vec3f refine = pixel - A, nrefine;
-            img_refine.at<cv::Vec3f>(i, j) = refine;
             cv::normalize(refine, nrefine);    //normalization, point to search
 
             float r = sqrt(nrefine[0] * nrefine[0] +
                             nrefine[1] * nrefine[1] +
                             nrefine[2] * nrefine[2]);
-
-            float * point = new float[2];
-            point[0] = acos(nrefine[2] / r);
-            point[1] = atan(nrefine[1] / nrefine[0]);
-            cvflann::Matrix<float> query(point, 1, 2);
-
-            cvflann::Matrix<int> vecIndex(new int, 1, 1);
-            cvflann::Matrix<float> vecDist(new float, 1, 1);
-            // kdtree.knnSearch(Mat(point).reshape(1), vecIndex, vecDist, 1);
-            kdtree.knnSearch(query, vecIndex, vecDist, 1, cvflann::SearchParams(1));
-            img_index[i][j] = vecIndex[0][0];
-
+            query_ar[count++] = acos(nrefine[2] / r);
+            query_ar[count++] = atan(nrefine[1] / nrefine[0]);
             dist[i][j] = sqrt(refine[0] * refine[0] + refine[1] * refine[1] + refine[2] * refine[2]);
-            if (dist[i][j] > max_dist[vecIndex[0][0]])
-                max_dist[vecIndex[0][0]] = dist[i][j];
         }
     }
+
+    int num_query = img_scale.rows * img_scale.cols;
+    cvflann::Matrix<float> query(query_ar, num_query, 2);
+
+    cvflann::Matrix<int> vecIndex(new int[num_query], num_query, 1);
+    cvflann::Matrix<float> vecDist(new float[num_query], num_query, 1);
+    kdtree.knnSearch(query, vecIndex, vecDist, 1, cvflann::SearchParams(1));
+    
     for (int i = 0; i < img_scale.rows; ++i) {
         for (int j = 0; j < img_scale.cols; ++j) {
-            T.at<float>(i, j) = dist[i][j] / max_dist[img_index[i][j]];
+            int ind = vecIndex[i * img_scale.cols + j][0];
+            if (dist[i][j] > max_dist[ind])
+                max_dist[ind] = dist[i][j];
+        }
+    }
+
+    for (int i = 0; i < img_scale.rows; ++i) {
+        for (int j = 0; j < img_scale.cols; ++j) {
+            int ind = vecIndex[i * img_scale.cols + j][0];
+            T.at<float>(i, j) = dist[i][j] / max_dist[ind];
         }
     }
 
